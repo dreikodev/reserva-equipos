70886c54a4db28b86ae3fec4ead3b0d3a3f7d987
Francis <francisespinoza152@gmail.com>
2026-05-08 23:00:23 -0500

    final

--- script.js
+++ script.js
@@ -1,5 +1,28 @@
-const btnRegistrar = document.getElementById('btnRegistrar')
+const btnRegistrar = document.getElementById('btnRegistrar');
 
-btnRegistrar.addEventListener('click', function(){
+btnRegistrar.addEventListener('click', function(e){
+
+    e.preventDefault();
+
+    const nombre = document.getElementById('name').value;
+    const area = document.getElementById('area').value;
+    const equipo = document.getElementById('equipo').value;
+    const fecha = document.getElementById('fecha').value;
+    const hora = document.getElementById('hora').value;
+    const lugar = document.getElementById('lugar').value;
+
+    const fila = `
+    <tr>
+        <td>${nombre}</td>
+        <td>${area}</td>
+        <td>${equipo}</td>
+        <td>${fecha}</td>
+        <td>${hora}</td>
+        <td>${lugar}</td>
+    </tr>
+    `;
+
+    document.getElementById("tablaReservas").innerHTML =
+    fila + document.getElementById("tablaReservas").innerHTML;
 
 });
